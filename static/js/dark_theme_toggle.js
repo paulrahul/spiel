961993c1f0af39c439f219a6edf73707a4835d41
Rahul Paul <paul.rahul@gmail.com>
2024-05-21 01:41:37 +0200

    Added toggle button and fixed stats display.

---
+++ static/js/dark_theme_toggle.js
@@ -0,0 +1,35 @@
+// Function to toggle the dark theme
+function toggleTheme() {
+    const body = document.body;
+    const currentTheme = body.classList.toggle('dark-theme');
+    
+    // Save the user's preference in localStorage
+    localStorage.setItem('dark-theme', currentTheme ? 'enabled' : 'disabled');
+}
+
+// // Add event listener to the toggle button
+// document.getElementById('theme-toggle').addEventListener('click', toggleTheme);
+
+// Check the user's preference on page load
+// window.onload = function() {
+//     const darkThemeEnabled = localStorage.getItem('dark-theme') === 'enabled';
+//     if (darkThemeEnabled) {
+//         document.body.classList.add('dark-theme');
+//     }
+// };
+
+document.getElementById('toggle').addEventListener('change', function() {
+    toggleTheme();
+});
+
+var toggleElem = document.getElementById('toggle');
+if (toggleElem != undefined) {
+    const darkThemeEnabled = localStorage.getItem('dark-theme') === 'enabled';
+    if (darkThemeEnabled) {
+        document.body.classList.add('dark-theme');
+        var checkbox = document.getElementById('toggle');
+        checkbox.checked = true;
+    }
+}
+
+
