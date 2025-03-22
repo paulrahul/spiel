c991061ec0e8882797f550fede431ce2f49c51f5
Rahul Paul <paul.rahul@gmail.com>
2025-03-22 14:54:46 +0100

    Fixes.

--- app.py
+++ app.py
@@ -2,12 +2,13 @@ import os, sys
 sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 
 from flask import Flask, abort, redirect, render_template, request, url_for, jsonify
+from flask_cors import CORS
 from functools import wraps
 from werkzeug.utils import redirect
 import uuid
 
 from .spiel import DeutschesSpiel
-from .translation_compiler import Compiler
+# from .translation_compiler import Compiler
 
 spiel = DeutschesSpiel(use_semantic=False, use_multimode=True)
 
@@ -17,6 +18,8 @@ def _generate_session_id():
 
 def create_app():
     app = Flask(__name__)
+    
+    CORS(app)
 
     app.config.from_mapping(
         DEEPL_KEY = os.environ.get("DEEPL_KEY"),
@@ -33,7 +36,7 @@ def create_app():
         if session_id and session_id != app.config["SESSION_ID"]:
             return False
         return True
-    
+
     def dec_validate_session_id(f):
         @wraps(f)
         def decorated_function(*args, **kwargs):
@@ -48,14 +51,14 @@ def create_app():
             "index.html",
             prod_mode=is_prod_mode())
 
-    @app.route("/reload")
-    def reload():
-        compiler = Compiler(app.config["DEEPL_KEY"])
-        compiler.scrape_new(reload=True)
-        return render_template(
-            "index.html",
-            reloaded=True)
-        
+    # @app.route("/reload")
+    # def reload():
+    #     compiler = Compiler(app.config["DEEPL_KEY"])
+    #     compiler.scrape_new(reload=True)
+    #     return render_template(
+    #         "index.html",
+    #         reloaded=True)
+
     @app.route('/init', methods=['POST'])
     def init():
         # Receive the data sent in the POST request
@@ -83,7 +86,7 @@ def create_app():
         
         data = {'session_id': app.config["SESSION_ID"]}
         return jsonify(data), 200
-        
+
     @app.route("/play")
     def play():
         return redirect(url_for('next_question'))
@@ -121,7 +124,7 @@ def create_app():
             start=start,
             entry=next_question,
             session_id=app.config["SESSION_ID"])
-        
+      
     @app.route("/lookup")
     def lookup():
         query_param = request.args.get('wort')
@@ -129,17 +132,25 @@ def create_app():
         if query_param:
             result = spiel.lookup(query_param)
 
-        return render_template(
-            "lookup.html",
-            word=query_param,
-            entry=result)
-        
+        is_json = request.args.get('mode') == "json"
+        if is_json:
+            if result is None:
+                return jsonify({'examples': []}), 200            
+            
+            data = {'examples': result['examples']}
+            return jsonify(data), 200
+        else:
+            return render_template(
+                "lookup.html",
+                word=query_param,
+                entry=result)
+
     @app.route("/list")
     def list():
         word_list = spiel.list()
         word_list = [word.capitalize() for word in word_list]
         return jsonify(sorted(word_list)), 200
-        
+     
     @app.route("/answer_score")
     def answer_score():
         user_answer = request.args.get('answer')
